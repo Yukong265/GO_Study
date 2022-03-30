286f79b15947d83e87fed87fc1b80a718f5b880c
Yu Bin <subenmoom50@gmail.com>
2022-03-30 19:21:26 +0900

    return and error handler

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,14 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	message := fmt.Sprintf("안녕하세요, %v. 환영합니다!", name)
-	return message
+	return message, nil
 }
